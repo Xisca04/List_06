a068e3f5a439976bf842357ace6d26d33b27395b
Xisca04 <xiscacerdavicens@gmail.com>
2023-02-06 10:39:50 +0100

    ex_4_not_end

---
+++ Assets/Scripts/Exercise_4.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise_4 : MonoBehaviour
+{
+    // All multiples of 5 between the start and the end
+
+    public int start = 2;
+    public int end = 37;
+
+    private void Start()
+    {
+        
+    }
+}
